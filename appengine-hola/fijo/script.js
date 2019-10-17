49aa3747b136f400d836e5dabdaa7cb9fc87beb3
JeiHO <21228328+jeijei4@users.noreply.github.com>
2019-10-17 14:39:04 -0600

    Update script.js

--- appengine-hola/fijo/script.js
+++ appengine-hola/fijo/script.js
@@ -1,24 +1,21 @@
 /*
 Copyright 2015 Google Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+Liberado bajo la Licencia Apache, versión 2.0 (la "Licencia");
+no puedes utilizar este archivo salvo en conformidad con la Licencia.
+Puedes obtener una copia de la Licencia en:
+http://www.apache.org/licenses/LICENSE-2.0
+A menos que lo requiera la ley aplicable o se acuerde por escrito, el
+software distribuido bajo la licencia se distribuye "TAL CUAL", SIN
+GARANTÍAS NI CONDICIONES DE NINGÚN TIPO, ya sean expresas o implícitas.
+Consulta la licencia para el idioma específico que rige los permisos y
+limitaciones conforme a la licencia.
 */
 
 "use strict";
 
-function fetchMessage() {
+function recuperaMensaje() {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "/hello", false);
+    xmlHttp.open("GET", "/hola", false);
     xmlHttp.send(null);
-    document.getElementById("message").innerHTML = xmlHttp.responseText;
+    document.getElementById("mensaje").innerHTML = xmlHttp.responseText;
 }
